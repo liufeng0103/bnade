3caaae69c07704ecf00bdbc5f34077d8c994e375
Feng Liu <liufeng0103@163.com>
2016-08-12 08:56:28 +0800

    1. 新增搜索物品api 搜索为模糊查询 结果包含物品和宠物信息

--- src/main/java/com/bnade/wow/service/ItemService.java
+++ src/main/java/com/bnade/wow/service/ItemService.java
@@ -4,13 +4,15 @@ import java.sql.SQLException;
 import java.util.List;
 
 import com.bnade.wow.po.Item;
+import com.bnade.wow.po.ItemV;
 
 public interface ItemService {
 	
 	List<Item> getItemsByName(String name) throws SQLException;
 	
-	List<Item> getItemsByName(String name, boolean isFuzzy, int limit) throws SQLException;
+	List<Item> getItemsByName(String name, boolean isFuzzy, int offset, int limit) throws SQLException;
 	
 	Item getItemById(int id) throws SQLException;
 	
+	List<ItemV> get(String name, int offset, int limit) throws SQLException;
 }
