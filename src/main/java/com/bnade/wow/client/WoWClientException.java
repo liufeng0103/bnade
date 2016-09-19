2e9f5c29605630284b68fbf0f1e0f48f4472c3df
Feng Liu <liufeng0103@163.com>
2016-09-19 12:19:13 +0800

    修改类名

---
+++ src/main/java/com/bnade/wow/client/WoWClientException.java
@@ -0,0 +1,14 @@
+package com.bnade.wow.client;
+
+public class WoWClientException extends Exception {
+
+	private static final long serialVersionUID = -6279921800735861381L;
+
+	public WoWClientException() {
+		super();
+	}
+	
+	public WoWClientException(Throwable e) {
+		super(e);
+	}
+}
