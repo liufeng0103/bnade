0cc60dcd2478f76a4023ecd045ffff3376b8089b
Feng Liu <liufeng0103@163.com>
2016-10-10 20:01:13 +0800

    jersey2切换会jersey1

--- src/main/java/com/bnade/wow/BnadeApplication.java
+++
@@ -1,9 +0,0 @@
-package com.bnade.wow;
-
-import org.glassfish.jersey.server.ResourceConfig;
-
-public class BnadeApplication extends ResourceConfig {
-	public BnadeApplication() {  
-        packages(true, "com.bnade.wow.rs");  
-    } 
-}
